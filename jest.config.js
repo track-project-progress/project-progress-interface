28adb517fc6253458092343079acda58a59227db
Mike Mwanje <mwanjemike767@gmail.com>
2021-10-09 18:38:58 +0300

    jest config

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,6 @@
 module.exports = async () => {
   return {
     verbose: true,
-    transform: { '^.+\\.[t|j]sx?$': 'babel-jest' },
     testEnvironment: 'node',
     coveragePathIgnorePatterns: ['/node_modules/', '/src/'],
     testMatch: ['**/test/**/*test.js'],
