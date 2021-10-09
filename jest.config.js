2bb1b5a8b4df65bd35a1f862c1cc3f09717fc43b
Mike Mwanje <mwanjemike767@gmail.com>
2021-10-09 18:41:59 +0300

    test with github actions on push

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = async () => {
   return {
     verbose: true,
+    transform: { '^.+\\.[t|j]sx?$': 'babel-jest' },
     testEnvironment: 'node',
     coveragePathIgnorePatterns: ['/node_modules/', '/src/'],
     testMatch: ['**/test/**/*test.js'],
